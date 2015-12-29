984dfb1dcbfdc04f80f97e30ead22a038ded638c
Kevin Fink <kevin.fink@rightside.co>
2015-12-28 16:24:38 -0800

    Use a valid version format

--- social/__init__.py
+++ social/__init__.py
@@ -2,6 +2,6 @@
 python-social-auth application, allows OpenId or OAuth user
 registration/authentication just adding a few configurations.
 """
-version = (0, 2, 13, 'kf')
+version = (0, 2, 13, 1)
 extra = ''
 __version__ = '.'.join(map(str, version)) + extra
